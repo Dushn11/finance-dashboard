13c9e01da438a0fc53a0afc379c2a944f32ff745
Dushn11 <m.a.bilous2010@gmail.com>
2026-04-29 09:31:40 +0200

    recreated

--- finance-dashboard/src/app/pages/import/import.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-import',
-  imports: [],
-  templateUrl: './import.html',
-  styleUrl: './import.scss',
-})
-export class Import {}
