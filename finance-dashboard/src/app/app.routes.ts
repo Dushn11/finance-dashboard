3472e5d74b26e37d082291b2af4fd25cd8b624c7
Dushn11 <m.a.bilous2010@gmail.com>
2026-04-29 10:14:03 +0200

    routing

--- finance-dashboard/src/app/app.routes.ts
+++ finance-dashboard/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Settings } from './pages/settings/settings';
+import { Import } from './pages/import/import';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Dashboard,
+        title: 'Dashboard'
+    },
+    {
+        path: 'settings',
+        component: Settings,
+        title: 'Settings'
+    },
+    {
+        path: 'import',
+        component: Import,
+        title: 'Import'
+    }
+];
