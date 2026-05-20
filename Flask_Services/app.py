281143c6f6bcdaeee3aa45e1f291949e5ae4400d
nttt <oliwier.drazkowski@gmail.com>
2026-05-20 08:31:31 +0200

    cleaning up
    put everything into 1 folder

---
+++ Flask_Services/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from routes.import_routes import import_bp
+
+def create_app():
+    app=Flask(__name__)
+    app.register_blueprint(import_bp, url_prefix="/import")
+    return app
+app = create_app()
+if __name__ =="__main__":
+    app.run(debug=True)
---
+++ Flask_Services/services/csv_service.py
@@ -0,0 +1,17 @@
+import csv
+import io
+
+def parse_csv(file):
+    if not file:
+        return []
+    try:
+        stream=io.TextIOWrapper(file.stream,encoding="utf-8")
+        reader = csv.DictReader(stream)
+
+        transactions =[row for row in reader]
+        return transactions
+    except Exception as e:
+        print(f"CSV parsing error: {e}")
+        return []
+
+    
