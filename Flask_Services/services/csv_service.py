920c6a93d460c89725cf0a956f687ab9da4f84ab
nttt <oliwier.drazkowski@gmail.com>
2026-05-27 09:44:44 +0200

    New parsing format test

--- Flask_Services/services/csv_service.py
+++ Flask_Services/services/csv_service.py
@@ -1,17 +1,25 @@
 import csv
 import io
 
+
 def parse_csv(file):
     if not file:
         return []
-    try:
-        stream=io.TextIOWrapper(file.stream,encoding="utf-8")
-        reader = csv.DictReader(stream)
 
-        transactions =[row for row in reader]
-        return transactions
+    try:
+        stream = io.TextIOWrapper(file.stream, encoding="utf-8")
+        first_line = stream.readline()
+
+        stream.seek(0)
+
+        if "Amount" in first_line:
+            reader = csv.DictReader(stream)
+            return [row for row in reader]
+
+        else:
+            reader = csv.reader(stream)
+            return [row for row in reader]
+
     except Exception as e:
         print(f"CSV parsing error: {e}")
         return []
-
-    
--- Flask_Services/services/normalization_service.py
+++ Flask_Services/services/normalization_service.py
@@ -1,17 +1,55 @@
+def parse_amount(value):
+    if not value:
+        return 0.0
+
+    return float(
+        str(value)
+        .replace('"', '')
+        .replace(',', '.')
+        .strip()
+    )
+
 
 def normalize_data(rows):
-    normalized =[]
+    normalized = []
+
+    if not rows:
+        return normalized
+
+    is_standard_format = isinstance(rows[0], dict)
+
     for r in rows:
         try:
-            normalized.append({
-                "amount": float(r.get("Amount",0.0)),
-                "description": r.get("Description",""),
-                "type":r.get("type","INCOME")
-            })
-                
-            
+
+            if is_standard_format:
+
+                normalized.append({
+                    "amount": float(r.get("Amount", 0.0)),
+                    "description": r.get("Description", ""),
+                    "type": r.get("type", "INCOME")
+                })
+
+            else:
+
+                description = r[3].strip()
+
+                expense = parse_amount(r[10])
+                income = parse_amount(r[11])
+
+                if income > 0:
+                    amount = income
+                    tx_type = "INCOME"
+                else:
+                    amount = -expense
+                    tx_type = "EXPENSE"
+
+                normalized.append({
+                    "amount": amount,
+                    "description": description,
+                    "type": tx_type
+                })
+
         except Exception as e:
-            print("Normalization error", e)
-            continue
+            print("Normalization error:", e)
 
     return normalized
