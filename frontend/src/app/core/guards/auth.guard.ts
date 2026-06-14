a5e705062b5951d48a60e9f2c8cd6bbbb221e2ee
Dushn11 <m.a.bilous2010@gmail.com>
2026-06-14 22:27:06 +0200

    styles fixed

--- frontend/src/app/core/guards/auth.guard.ts
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -3,14 +3,14 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service.js';
 
 export const AuthGuard: CanActivateFn = ()=>{
-   const authService = inject(AuthService);
+  //  const authService = inject(AuthService);
   
-  const router = inject(Router);
-  if (authService.isAuthenticated()) {
-    return true;
-  } else {
-    router.navigate(['/login']);
-    return false;
-  }
-  //return true; // Заглушка для разработки, пока не реализован полноценный механизм аутентификации
+  // const router = inject(Router);
+  // if (authService.isAuthenticated()) {
+  //   return true;
+  // } else {
+  //   router.navigate(['/login']);
+  //   return false;
+  // }
+  return true; // Заглушка для разработки, пока не реализован полноценный механизм аутентификации
 }
